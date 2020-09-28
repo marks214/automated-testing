410bb033eb198abf95d16d7b02301eef297c206d
marks214 <aimee.oz.x@gmail.com>
2020-09-28 15:54:41 -0700

    how far I got before I got stuck and watched Chris' live-code video

--- lib/card.rb
+++ lib/card.rb
@@ -4,15 +4,33 @@
 class Card
   attr_reader :value, :suit
 
+  CARDS_IN_DECK = [:hearts, :spades, :clubs, :diamonds] + (1..13).to_a
+
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
+    raise ArgumentError.new("invalid card") unless CARDS_IN_DECK.include? @value
 
+  end
+
+  def convert_num_to_face(number)
+    return 'Ace' if number == 1
+    return 'Jack' if number == 11
+    return 'King' if number == 12
+    return 'Queen' if number == 13
+    return number
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{convert_num_to_face(value)} of #{suit.to_s}"
   end
 
+  def self.value
+    card = self.new(@value, @suit)
+    return @value
+  end
+
+  def self.suit
+
+  end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -1,9 +1,9 @@
 require_relative 'test_helper'
 
 describe Card do
-  describe "You can create a Card instance" do
+  describe 'You can create a Card instance' do
 
-    it "Can be created" do
+    it 'Can be created' do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
           card = Card.new(value, suit)
@@ -12,7 +12,7 @@ describe Card do
       end
     end
 
-    it "Raises an ArgumentError for invalid parameters" do
+    it 'Raises an ArgumentError for invalid parameters' do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
       expect {Card.new(0, :diamonds)}.must_raise ArgumentError
@@ -21,14 +21,34 @@ describe Card do
     end
   end
 
-  describe "Testing to_s" do
+  describe 'Testing to_s' do
 
-    it "to_s returns a readable String value logically for values 2-10" do
+    it 'to_s returns a readable String value logically for values 2-10' do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (2..10).each do |value|
+          card = Card.new(value, suit)
+          expect(card.to_s).must_equal "#{value} of #{suit.to_s}"
+        end
+      end
     end
 
-    it "to_s returns a readable String value for Ace, Jack, Queen, King" do
+    it 'convert_num_to_face returns strings Ace, Jack, Queen, or King when given integers
+        1, 11, 12, or 13, respectively - otherwise the numerical card value is returned' do
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        [1, 2, 11, 12, 13].each do |value|
+          card = Card.new(value, suit)
+          expect(card.convert_num_to_face(1)).must_equal 'Ace'
+          expect(card.convert_num_to_face(11)).must_equal 'Jack'
+          expect(card.convert_num_to_face(12)).must_equal 'King'
+          expect(card.convert_num_to_face(13)).must_equal 'Queen'
+          expect(card.convert_num_to_face(2)).must_equal 2
+        end
+      end
+    end
+
+    it 'to_s returns a readable String value for Ace, Jack, Queen, King' do
       # Test to ensure that to_s works for cards values 1, and 11-13
       # For example: "Queen of hearts"
       # The current implementation of to_s does not address this feature
@@ -40,16 +60,22 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        [1, 11, 12, 13].each do |value|
+          card = Card.new(value, suit)
+          expect(card.to_s).must_equal "#{card.convert_num_to_face(value)} of #{suit.to_s}"
+        end
+      end
     end
   end
 
-  describe "Reader methods" do
+  describe 'Reader methods' do
 
-    it "Can retrieve the value of the card using a `.value`." do
+    it 'Can retrieve the value of the card using a `.value`.' do
       # ensure that `.value works as expected`
     end
 
-    it "Can retrieve the value of the card using a `.suit`." do
+    it 'Can retrieve the value of the card using a `.suit`.' do
       # ensure that `.suit works as expected returning the symbol of the suit`
 
     end
